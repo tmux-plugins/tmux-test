33fa65fbfb72ba6dd106c21bf5ee6cc353ecdbb6
Bruno Sutic <bruno.sutic@gmail.com>
2015-08-04 17:02:23 +0200

    Bugfix vagrant centos provisioning

--- vagrant_centos_provisioning.sh
+++ vagrant_centos_provisioning.sh
@@ -22,4 +22,5 @@ git clone https://github.com/tmux/tmux.git ~/tmux_source
 cd ~/tmux_source
 git checkout 2.0
 sh autogen.sh
-./configure && make && sudo make install
+LDFLAGS="-L/usr/local/lib -Wl,-rpath=/usr/local/lib" ./configure --prefix=/usr/local
+make && sudo make install
